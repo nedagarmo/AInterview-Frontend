f8329e41db9dbff38501ee238a5d784c1006b8ce
Nelson Garzón <nedagarmo@gmail.com>
2020-12-07 16:38:09 -0500

    feat: initial modules

---
+++ src/app/interview/interview.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MeetComponent } from './meet/meet.component';
+
+
+
+@NgModule({
+  declarations: [MeetComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class InterviewModule { }
---
+++ src/app/interview/meet/meet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meet',
+  templateUrl: './meet.component.html',
+  styleUrls: ['./meet.component.scss']
+})
+export class MeetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
